0fc190e17f754975b623691812d15d7a9406e468
20210181 <20210181@kumoh.ac.kr>
2025-11-12 15:46:00 +0900

    🏗️MVVM

---
+++ S25W11List/ViewModel/SongViewModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+@Observable
+final class SongViewModel {
+    private var _songs: [Song] = []
+    
+    var songs: [Song] {
+        _songs
+    }
+    
+    func add(song: Song) {
+        _songs.append(song)
+    }
+}
