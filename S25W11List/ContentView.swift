80d1e484d25aabe68285ba0391537dab57489c35
20210181 <20210181@kumoh.ac.kr>
2025-11-17 16:10:19 +0900

    🎨컴포넌트 분리

--- S25W11List/ContentView.swift
+++ S25W11List/ContentView.swift
@@ -12,23 +12,49 @@ struct MyList: View {
     var body: some View {
         List {
             ForEach(viewModel.songs, id: \.id) { song in
-                VStack {
-                    Text(song.title)
-                        .font(.headline)
-                        .foregroundColor(.blue)
-                        .padding()
-                    Text(song.singer)
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                        .padding()
-                }
+                SongItem(song: song)
             }
+            
         }
         .onAppear {
             viewModel.add(song: Song(id: 1, title: "Golden", singer: "HUNTR/X"))
             viewModel.add(song: Song(id: 2, title: "Drowning", singer: "WOODZ"))
             viewModel.add(song: Song(id: 3, title: "Soda Pop", singer: "Saja Boys"))
         }
+    }
+}
+
+struct SongItem: View {
+    let song: Song
+    
+    var body: some View {
+        VStack {
+            TextTitle(title: song.title)
+            TextSinger(singer: song.singer)
+        }
+        .padding()
+    }
+}
+
+struct TextTitle: View {
+    let title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.headline)
+            .foregroundColor(.blue)
+            .padding()
+    }
+}
+
+struct TextSinger: View {
+    let singer: String
+    
+    var body: some View {
+        Text(singer)
+            .font(.caption)
+            .foregroundColor(.gray)
+            .padding()
     }
 }
 
